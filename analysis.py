72f6cb8e3aff1d9c02f7e41d10c0c10c9f6f778e
Genz0819 <bonethugs15@yahoo.com>
2022-08-24 15:43:29 +0100

    Updated README with new features

--- analysis.py
+++ analysis.py
@@ -6,3 +6,4 @@ Temporary commit content for 2022-07-19T15:43:29
 Temporary commit content for 2022-07-23T15:43:29
 Temporary commit content for 2022-07-31T15:43:29
 Temporary commit content for 2022-08-14T15:43:29
+Temporary commit content for 2022-08-24T15:43:29
