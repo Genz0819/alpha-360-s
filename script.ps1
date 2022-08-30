17fe08215300e251a37c392acde066210fce7352
Genz0819 <bonethugs15@yahoo.com>
2022-08-30 15:43:29 +0100

    Added validation for user input

--- script.ps1
+++ script.ps1
@@ -3,3 +3,4 @@ Temporary commit content for 2022-07-16T15:43:29
 Temporary commit content for 2022-08-07T15:43:29
 Temporary commit content for 2022-08-23T15:43:29
 Temporary commit content for 2022-08-28T15:43:29
+Temporary commit content for 2022-08-30T15:43:29
