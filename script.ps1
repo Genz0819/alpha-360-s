6eb34408f45e0bd1409a54904224c8fbe8c4eb9e
Genz0819 <bonethugs15@yahoo.com>
2023-08-12 15:43:29 +0100

    Fixed typos in documentation

--- script.ps1
+++ script.ps1
@@ -23,3 +23,4 @@ Temporary commit content for 2023-06-23T15:43:29
 Temporary commit content for 2023-06-29T15:43:29
 Temporary commit content for 2023-07-11T15:43:29
 Temporary commit content for 2023-07-21T15:43:29
+Temporary commit content for 2023-08-12T15:43:29
