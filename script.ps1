2cd97b014f3cd6a7667ffc251d36ded85f7e46fc
Genz0819 <bonethugs15@yahoo.com>
2023-10-21 15:43:29 +0100

    Refactored code for better readability

--- script.ps1
+++ script.ps1
@@ -30,3 +30,4 @@ Temporary commit content for 2023-10-03T15:43:29
 Temporary commit content for 2023-10-05T15:43:29
 Temporary commit content for 2023-10-09T15:43:29
 Temporary commit content for 2023-10-11T15:43:29
+Temporary commit content for 2023-10-21T15:43:29
