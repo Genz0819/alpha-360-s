8216ac743e4710f1f2676aaad6877f5753766d59
Genz0819 <bonethugs15@yahoo.com>
2024-05-11 15:43:29 +0100

    Updated README with new features

--- script.ps1
+++ script.ps1
@@ -46,3 +46,4 @@ Temporary commit content for 2024-04-08T15:43:29
 Temporary commit content for 2024-04-25T15:43:29
 Temporary commit content for 2024-05-06T15:43:29
 Temporary commit content for 2024-05-07T15:43:29
+Temporary commit content for 2024-05-11T15:43:29
