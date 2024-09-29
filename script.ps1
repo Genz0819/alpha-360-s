300d4e990761f8878df112ddbe5952965fb357d1
Genz0819 <bonethugs15@yahoo.com>
2024-09-29 15:43:29 +0100

    Added validation for user input

--- script.ps1
+++ script.ps1
@@ -59,3 +59,4 @@ Temporary commit content for 2024-09-01T15:43:29
 Temporary commit content for 2024-09-03T15:43:29
 Temporary commit content for 2024-09-14T15:43:29
 Temporary commit content for 2024-09-18T15:43:29
+Temporary commit content for 2024-09-29T15:43:29
