fd097f4614b136fa2bd9b48348727a85f329703f
Genz0819 <bonethugs15@yahoo.com>
2024-12-10 15:43:29 +0100

    Refactored code for better readability

--- analysis.py
+++ analysis.py
@@ -71,3 +71,4 @@ Temporary commit content for 2024-10-23T15:43:29
 Temporary commit content for 2024-11-04T15:43:29
 Temporary commit content for 2024-11-18T15:43:29
 Temporary commit content for 2024-11-28T15:43:29
+Temporary commit content for 2024-12-10T15:43:29
